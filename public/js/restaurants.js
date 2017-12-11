e7056f589aed36f3af8d36d02b7b18196c13f05c
bobo <>
2017-12-11 15:17:56 -0500

    css for mainpage restaurant

---
+++ public/js/restaurants.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(".review").each(function(i){
+        var divH = $(this).height();
+        var $p = $("p", $(this)).eq(0);
+        while ($p.outerHeight() > divH) {
+            $p.text($p.text().replace(/(\s)*([a-zA-Z0-9]+|\W)(\.\.\.)?$/, "..."));
+        };
+    });
+})(window.jQuery);
