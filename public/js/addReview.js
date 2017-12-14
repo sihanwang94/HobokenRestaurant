b0145d467539352bb5563fd2653f2ed6ec5d4014
einsteinder <einsteinder@gmail.com>
2017-12-14 16:01:55 -0500

    Restaurant not found.

--- public/js/addReview.js
+++ public/js/addReview.js
@@ -1,12 +1,42 @@
-/*var form = document.querySelector("form");
-var log = document.querySelector("#pre");
+// magic.js
+$(document).ready(function() {
 
-form.addEventListener("submit", function(event) {
-  var data = new FormData(form);
-  var output = "";
-  for (const entry of data) {
-    output = entry[0] + "=" + entry[1] + "\r";
-  };
- pre.innerText = output;
-  event.preventDefault();
-}, false);*/
+      // process the form
+      $('form').submit(function(event) {
+        
+  
+          // get the form data
+          // there are many ways to get this data using jQuery (you can use the class or id also)
+          var formData = {
+              'reviewer_like'      : $('input[name=score]:checked').val(),
+              'review'             : $('textarea[name=comment]').val()
+          };
+          var restaurantId='/reviews/'+$('#restaurantId').text()
+          console.log(restaurantId)
+          console.log(formData)
+          // process the form
+          $.ajax({
+              type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+              url         : restaurantId, 
+              data        : formData, // our data object
+              success: function(data) {
+                $('.text').text(JSON.stringify(data));
+              },
+              dataType    : 'json', // what type of data do we expect back from the server
+              encode      : true
+          })
+              // using the done promise callback
+              .done(function(data) {
+  
+                  // log data to the console so we can see
+                  console.log(data); 
+  
+                  // here we will handle errors and validation messages
+              });
+  
+          // stop the form from submitting the normal way and refreshing the page
+          event.preventDefault();
+      });
+  
+  });
+  
--- routes/reviews.js
+++ routes/reviews.js
@@ -182,7 +182,8 @@ router.get("/:restaurantId/:reviewId", async (req,res)=>{
 
 router.post("/:restaurantId", async (req,res)=>{
     let restaurantInfo=req.body;
-    let userInfo = req.user
+    let userInfo = req.user;
+    let restaurantId=req.params.restaurantId
     if(!restaurantInfo){
         res.status(400).json({error:"You must provide data to create a review."});
         return;
@@ -196,7 +197,7 @@ router.post("/:restaurantId", async (req,res)=>{
         return;
     }
     try {
-        const result=await reviewsData.addReview(userInfo._id,req.params.restaurantId,userInfo.local.email,restaurantInfo.reviewer_like,restaurantInfo.review);
+        const result=await reviewsData.addReview(userInfo._id,restaurantId,userInfo.local.email,restaurantInfo.reviewer_like,restaurantInfo.review);
         res.json(result);
     } catch (e){
         console.log(e);
