563511750d7751c84d433b163d85ea02975e3589
bobo <>
2017-12-15 13:04:40 -0500

    single map add marker

--- public/js/nearby.js
+++ public/js/nearby.js
@@ -3,16 +3,10 @@ $(document).ready(function() {
     var map;
     var places;
     var markers = [];
-    var bindInfoWindow = function(marker, map, infowindow) {
-	    google.maps.event.addListener(marker, 'click', function() {
-	        infowindow.setContent(html);
-	        infowindow.open(map, marker);
-	    });
-	} 
+    
+    geocoder = new google.maps.Geocoder();
 
     function initialize() {
-        geocoder = new google.maps.Geocoder();
-    
         var mapOptions = {
             center: new google.maps.LatLng(40.7441,-74.0324),
             zoom: 14,
@@ -21,31 +15,45 @@ $(document).ready(function() {
     
         map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
     }
+
     // when page is ready, initialize the map!
     google.maps.event.addDomListener(window, 'load', initialize);
 
     var address=$('#locationId').text();
     var urlData=address.toString().split(' ').join('+');
     console.log(urlData);
+    var name=$('#locationName').text();
+    console.log(name);
+    var infowindow =  new google.maps.InfoWindow({
+        content: name
+    });
+
+    var bindInfoWindow = function(marker, map, infowindow) {
+	    google.maps.event.addListener(marker, 'click', function() {
+	        infowindow.setContent(html);
+	        infowindow.open(map, marker);
+	    });
+	} 
               
     $.ajax({
         type: 'POST', 
         url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + urlData + "&key=" + "AIzaSyDDg770LxmiVd3xLpzpS7kri_aD6fKc2Is",
         data: address, 
         success: function(response) {
+            places = response.places;
             if (response.status == 'OK') {
                 geocoder.geocode( { 'address': address }, function(results, status) {
                     if (status == google.maps.GeocoderStatus.OK) {
-                        console.log(results);
+                        //console.log(results);
                         map.setCenter(results[0].geometry.location);
                         
                         var marker = new google.maps.Marker({
                             map: map,
-                            position: results[0].geometry.location
+                            position: results[0].geometry.location,
+                            html: address
                         });
         
                         bindInfoWindow(marker, map,infowindow);
-                        // not currently used but good to keep track of markers
                         markers.push(marker);
                     }
                 })
@@ -57,7 +65,6 @@ $(document).ready(function() {
         encode : true
     }).done(function(data) {
         console.log(data);
-    });
-
+    });   
 });
 
