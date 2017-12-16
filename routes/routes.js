7a2b64e4ecf83cf822d743d945a7127e8a595638
anchor <anchor@host-155-246-141-42.dhcp.stevens-tech.edu>
2017-12-15 20:20:05 -0500

    revert

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,6 @@ const router=require("express").Router();
 const data=require("../data");
 const restaurantsData=data.restaurants;
 const reviewsData=data.reviews;
-const User=require("../data/user")
-const review=require("../data/reviews")
 
 
 module.exports = function(app, passport) {
@@ -27,22 +25,14 @@ module.exports = function(app, passport) {
         app.get('/profile', isLoggedIn, async function(req, res) {
             try{
                 const reviews = await reviewsData.getReviewsByUserId(req.user._id)
-
-                res.render('profile', {
-                    user : req.user,
-                    reviews:reviews
-                });
-
                 if(!reviews){
                     res.render('profile',{user : req.user})
-                }else{
-                    res.render('profile', {
-                        user : req.user,
-                        reviews:reviews
-                    });
-                }
-
-
+                                    }else{
+                                        res.render('profile', {
+                                           user : req.user,
+                                           reviews:reviews
+                                       });
+                                   }
                 }catch(e){
                     console.log(e);
                     res.redirect('/');
@@ -85,73 +75,6 @@ module.exports = function(app, passport) {
                 failureRedirect : '/signup', // redirect back to the signup page if there is an error
                 failureFlash : true // allow flash messages
             }));
-
-            //=============to enter adminProfile==============
-            app.get('/ADM',(req,res)=>{
-                //======================================
-                 if(req.user.local.email==='admin'){
-                    res.render('admin/adminProfile',{});
-                    }else{
-                                res.render('login', { message: req.flash('errorMessage') });
-                         }
-                  });
-
-    //================get adminProfile==========
-    
-            app.get('/adminProfile', isLoggedIn, function(req, res) {
-                if(req.user) res.render('adminProfile',{});
-                else{
-                    res.render('login')
-                }
-            }); 
-            
-    //=============find a user to userSearch page==========         
-    
-            app.post('/result',(req,res)=>{
-                //======================================
-                if(req.user){
-                    User.findOne({'local.email' :  req.body.keyword},(err,user)=>{
-                    res.render('admin/userSearch',{user:user});
-                    res.render('body/result_err',{}
-                    )
-                    
-                });
-                }
-                else res.redirect('../login');
-                });
-    
-                //show userlist here
-        //===============find all users====================     
-    
-            app.post('/resultall',(req,res)=>{
-                if(req.user){
-                    User.find({}).toArray().then((allUsers)=>{
-                        console.log(allUsers);
-                        const result=[];
-                        for(let i=0;i<allUsers.length;i++){
-                            let content={
-                                _id:allUsers[i]._id,
-                                email:allUsers[i].local.email
-                            }
-                                result.push(content);
-                                }res.render('userList', {allusers: result});
-    
-                        })}else res.redirect('/login');  
-                    }
-        );
-    //==========view user review=================
-    app.post('/reviewHistory',(req,res)=>{
-        if(req.user){
-            const reviewlist=review.getReviewsByUserId(req.user._id);
-            res.render('userSearch',{reviewlist:reviewlist});
-            }else res.redirect('/login');  
-            }
-);
-    
-    };
-    
-
-   
     
     
     
